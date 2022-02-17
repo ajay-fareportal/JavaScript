adc65bf50c40535130940b1b4940c33ace8709ad
ajay-fareportal <ajay.mourya@fareportal.com>
2022-02-17 18:14:02 +0530

    Day 3 Activity

---
+++ 3rd Assignment/script.js
@@ -0,0 +1,20 @@
+
+let variable;
+document.getElementById("btn")
+    .onclick = function () {
+        variable = document.getElementById("textbox").value;
+        document.getElementById("value")
+            .innerText = "The value of variable is: " + variable;
+        console.log(variable);
+    }
+
+var x = setInterval(changeValue, 10000)
+function changeValue() {
+    variable = Math.floor(Math.random() * (100 - 1) + 1);
+
+    document.getElementById("textbox").value = variable;
+    document.getElementById("value")
+        .innerText = "The value of variable is: " + variable;
+    console.log(variable);
+}
+
