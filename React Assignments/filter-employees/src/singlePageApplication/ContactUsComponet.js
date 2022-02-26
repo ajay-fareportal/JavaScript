8745cbdb43411e000c083c83b70d082d12a16f8d
ajay-fareportal <ajay.mourya@fareportal.com>
2022-02-27 02:22:25 +0530

    Single Page CRUD Application

--- React Assignments/filter-employees/src/singlePageApplication/ContactUsComponet.js
+++ React Assignments/filter-employees/src/singlePageApplication/ContactUsComponet.js
@@ -4,11 +4,11 @@ function HelpComponent() {
   return (
     <div>
       <h1 className='display-5 mt-5 ms-5 text-success'>Contact Us</h1>
-      <p className='ms-5 mt-3 text-light'>
+      <p className='ms-5 mt-3 me-5 text-light'>
         Consectetur cillum esse ad Lorem amet non. Dolore magna tempor cupidatat incididunt et elit ipsum ipsum laborum. Eiusmod consectetur nisi consectetur incididunt ut deserunt est est duis in. Ut culpa velit minim amet sint duis est ut commodo non sit. Quis ad nulla non elit.
         Velit anim do velit velit exercitation officia. Qui voluptate eiusmod id excepteur nisi sit voluptate enim id. Do eu velit minim ipsum minim eiusmod enim laborum. Voluptate consequat tempor sit voluptate veniam irure sunt ad nisi dolore nulla aliqua quis.
       </p>
-      <p className='ms-5 text-secondary'>
+      <p className='ms-5 me-5 text-secondary'>
         Ut ipsum occaecat consectetur non adipisicing adipisicing. Est sunt ut enim consectetur velit aliquip magna veniam sint. In adipisicing ex reprehenderit minim duis id ullamco qui. Est veniam et sit cupidatat dolor Lorem ad et. Aliquip magna do commodo id et dolore id esse proident laboris nisi culpa laborum cupidatat. Ullamco magna dolor ex in anim sint in id amet amet. Aute aliqua deserunt dolor ipsum cupidatat dolore aliquip laboris ut tempor sit cillum.
 
         Minim exercitation ipsum excepteur sunt irure quis sint Lorem commodo aute nostrud ad. Elit consequat laboris Lorem elit. Magna dolor elit duis quis do. Culpa minim aliquip occaecat sint et cillum et ipsum ad id fugiat laboris elit et. Dolor velit eu nulla minim consectetur incididunt aliquip eiusmod sint laborum quis.
